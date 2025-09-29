58bcc148642f7df82d6a7b2279a380179ee1dbee
mosh1331 <shamil.tfora@gmail.com>
2025-09-29 06:27:35 +0530

    landing page fix

--- src/components/landing/LandingPageView.jsx
+++ src/components/landing/LandingPageView.jsx
@@ -90,11 +90,11 @@ const LandingPage = () => {
       <section className="py-16 bg-gray-50 text-center px-8">
         <h2 className="text-2xl font-semibold mb-4">Stay Updated</h2>
         <p className="text-gray-700 mb-6">Be the first to know about new arrivals and special offers.</p>
-        <div className="flex flex-col sm:flex-row gap-4 justify-center">
+        <div className="flex flex-col sm:flex-row gap-4 justify-center w-full" >
           <input
             type="email"
             placeholder="Enter your email"
-            className="px-4 py-2 border border-gray-300 rounded-full focus:outline-none w-80"
+            className="px-4 py-2 block w-full border border-gray-300 rounded-full focus:outline-none w-80"
           />
           <button className="bg-black text-white px-6 py-2 rounded-full">Subscribe</button>
         </div>
